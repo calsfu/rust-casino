1472546c212d65b480ed952f8d6ed8ed47917fd7
coleres2 <colerezi30@gmail.com>
2023-06-27 21:56:47 -0600

    Added photos for select page

--- src/routes/select/+page.svelte
+++ src/routes/select/+page.svelte
@@ -1,6 +1,8 @@
 <script>
-    
+    import Background from '../Background.svelte'
 </script>
+
+<Background/>
 
 <button>
     <a href="/">
@@ -11,16 +13,42 @@
 <h1>Choose a game</h1>
 <button>
     <a href="/games/blackjack">
-        Blackjack
+        <img src="src/lib/images/Casino-stock/blackjack.jpg" alt="Blackjack" width="300" height="200">
+        <div class="caption">Blackjack</div>
     </a>
 </button>
 <button>
     <a href="/games/slots">
-        Slots
+        <img src="src/lib/images/Casino-stock/slot-machines.jpg" alt="Blackjack" width="300" height="200">\
+        <div class="caption">Slots</div>
     </a>
 </button>
 <button>
     <a href="/games/roulette">
-        Roulette
+        
+        <img src="src/lib/images/Casino-stock/roulette.jpg" alt="Blackjack" width="300" height="200">
+        <div class="caption">Roulette</div>
     </a>
 </button>
+
+<style>
+    button {
+        background-color: #38c740;
+        cursor: pointer;
+        font-family: system-ui,-apple-system,system-ui,"Helvetica Neue",Helvetica,Arial,sans-serif;
+        font-size: 16px;
+        font-weight: 600;
+        justify-content: center;
+    }
+    button:hover {
+        background-color: #187224;
+    } 
+    a {
+        text-decoration: none;
+        color: white;
+    }
+    h1 {
+        color:#38c740;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    }
+</style>
